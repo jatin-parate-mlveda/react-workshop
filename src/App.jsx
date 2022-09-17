54f660f137e24a335a70f00dd07df09f5cdc873c
Jatin <jatin.parate@mlveda.com>
2022-09-17 19:10:16 +0530

    added form control

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState({ value: "", error: null });
+
+  const onNameChange = (e) => {
+    const newValue = e.target.value.trim();
+    const error = Boolean(newValue)
+      ? newValue === "invalid"
+        ? "Invalid value"
+        : null
+      : "Required!";
+    return setName({
+      value: newValue,
+      error,
+    });
+  };
+
   return (
-    <div className="container mt-3">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input
-          type="text"
-          class="form-control"
-          name="name"
-          id="name"
-          aria-describedby="nameHelp"
-          placeholder="Enter your name"
-        />
-        <small id="nameHelp" class="form-text text-muted">
-          You can enter your name
-        </small>
-      </div>
+    <div className="container mt-5">
+      <form
+        noValidate
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            autoComplete="off"
+            value={name.value}
+            onChange={onNameChange}
+            name="name"
+            type="text"
+            className={`form-control ${name.error ? "is-invalid" : ""}`}
+            id="name"
+            placeholder="Enter name"
+          />
+          {name.error && <div className="invalid-feedback">{name.error}</div>}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
